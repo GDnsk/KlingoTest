f7d21fbc50ff2eeb8384dca692e4a50a677cbc0a
Guilherme Daniluski <guilhermedaniluski@gmail.com>
2021-01-05 11:02:11 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KlingoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

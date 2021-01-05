d3faa685e44835dcfb87dbc916eb09dd2880bb58
Guilherme Daniluski <guilhermedaniluski@gmail.com>
2021-01-05 13:24:33 -0300

    Number Converter and Good Numbers Counter

---
+++ KlingoMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KlingoTest
+{
+    public static class KlingoMath
+    {
+        public static long ToNumber(string value)
+        {
+            long number = 0;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                char letter = value[i];
+                int letterValue = KlingoLanguage.alphabet.IndexOf(letter);
+                number += (long)Math.Pow(20, i) * letterValue;
+            }
+            return number;
+        }
+        public static bool CheckIfIsGoodNumber(long number)
+        {
+            // Must be equal or grater than 440566
+            if (number < 440566) return false;
+
+            // Must be divisible by 3
+            if (number % 3 != 0) return false;
+
+            return true;
+        }
+
+
+    }
+}
